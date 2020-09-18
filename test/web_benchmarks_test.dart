a868ba982d753c340529ac48fcaf0374f9721ae2
Tianguang Zhang <penn.zht@gmail.com>
2020-09-18 21:48:36 +0200

    Update paths in `web_benchmarks_test.dart`.

--- test/web_benchmarks_test.dart
+++ test/web_benchmarks_test.dart
@@ -4,8 +4,8 @@ import 'server/server.dart';
 
 Future<void> main() async {
   final taskResult = await runWebBenchmark(
-    macrobenchmarksDirectory: '.',
-    entryPoint: 'test/test_app/benchmarks/runner.dart',
+    macrobenchmarksDirectory: 'test/test_app',
+    entryPoint: 'benchmarks/runner.dart',
     useCanvasKit: false,
   );
   print(JsonEncoder.withIndent('  ').convert(taskResult.toJson()));
