e8a61de8024d9cf8e34fc978df493f6ba7757fa9
Tianguang Zhang <penn.zht@gmail.com>
2020-09-18 21:59:46 +0200

    Add test body.

--- test/web_benchmarks_test.dart
+++ test/web_benchmarks_test.dart
@@ -6,13 +6,11 @@ import 'server/server.dart';
 
 Future<void> main() async {
   test('Web benchmarks run successfully, returning correct keys', () async {
-
+    final taskResult = await runWebBenchmark(
+      macrobenchmarksDirectory: 'test/test_app',
+      entryPoint: 'lib/benchmarks/runner.dart',
+      useCanvasKit: false,
+    );
+    print(JsonEncoder.withIndent('  ').convert(taskResult.toJson()));
   });
-
-  final taskResult = await runWebBenchmark(
-    macrobenchmarksDirectory: 'test/test_app',
-    entryPoint: 'lib/benchmarks/runner.dart',
-    useCanvasKit: false,
-  );
-  print(JsonEncoder.withIndent('  ').convert(taskResult.toJson()));
 }
