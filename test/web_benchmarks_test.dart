f293108926371ca4d2cff2ec50480aacbd0bb28e
Tianguang Zhang <penn.zht@gmail.com>
2020-09-18 22:05:52 +0200

    Expand test body.

--- test/web_benchmarks_test.dart
+++ test/web_benchmarks_test.dart
@@ -11,6 +11,37 @@ Future<void> main() async {
       entryPoint: 'lib/benchmarks/runner.dart',
       useCanvasKit: false,
     );
+
+    expect(taskResult.succeeded, true);
+
+    for (final benchmarkName in ['scroll', 'page', 'tap']) {
+      for (final metricName in ['preroll_frame', 'apply_frame', 'drawFrameDuration']) {
+        for (final valueName in ['average', 'outlierAverage', 'outlierRatio', 'noise']) {
+          expect(
+            taskResult.data.keys,
+            contains('$benchmarkName.$metricName.$valueName'),
+          );
+
+          if (metricName == 'drawFrameDuration' &&
+              ['average', 'outlierRatio'].contains(valueName)) {
+            expect(
+              taskResult.benchmarkScoreKeys,
+              contains('$benchmarkName.$metricName.$valueName'),
+            );
+          }
+        }
+      }
+      expect(
+        taskResult.data.keys,
+        contains('$benchmarkName.totalUiFrame.average'),
+      );
+
+      expect(
+        taskResult.benchmarkScoreKeys,
+        contains('$benchmarkName.totalUiFrame.average'),
+      );
+    }
+
     print(JsonEncoder.withIndent('  ').convert(taskResult.toJson()));
   });
 }
