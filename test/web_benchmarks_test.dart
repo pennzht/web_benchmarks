e0fcebc8874985699fc0f2c32bb1b7fdfd6df665
Tianguang Zhang <penn.zht@gmail.com>
2020-09-18 21:59:33 +0200

    Add test body.

--- test/web_benchmarks_test.dart
+++ test/web_benchmarks_test.dart
@@ -1,8 +1,14 @@
 import 'dart:convert' show JsonEncoder;
 
+import 'package:flutter_test/flutter_test.dart';
+
 import 'server/server.dart';
 
 Future<void> main() async {
+  test('Web benchmarks run successfully, returning correct keys', () async {
+
+  });
+
   final taskResult = await runWebBenchmark(
     macrobenchmarksDirectory: 'test/test_app',
     entryPoint: 'lib/benchmarks/runner.dart',
